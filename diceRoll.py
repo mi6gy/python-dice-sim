b8bfe2cfe10b0f5e56b52733097ae4dd37df916b
Miguel Jimenez <mjimenez08@gmail.com>
2021-02-06 11:17:11 -0500

    added player 1 and rounds to game

--- diceRoll.py
+++ diceRoll.py
@@ -1,7 +1,19 @@
 import random
 
+def main():
+    player1 = 0
+    player2 = 0
+    rounds = 1
+
+    while rounds !=4:
+        print("Round " + str(rounds))
+        player1 = dice_roll()
+        print(player1)
+
+        rounds = rounds + 1
+
 def dice_roll():
     diceRoll = random.randint (1,6)
     return diceRoll
 
-print (dice_roll())
+main()
