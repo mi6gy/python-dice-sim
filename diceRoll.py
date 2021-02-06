4ffc9b3a828f386c1610bcf3c97acec0bcd92cbd
Miguel Jimenez <mjimenez08@gmail.com>
2021-02-06 11:46:02 -0500

    fixed syntax, added rounds, added print outs for game winner

--- diceRoll.py
+++ diceRoll.py
@@ -2,10 +2,12 @@ import random
 
 def main():
     player1 = 0
+    player1wins = 0
     player2 = 0
+    player2wins = 0
     rounds = 1
 
-    while rounds !=4:
+    while rounds !=10:
         print("Round " + str(rounds))
         player1 = dice_roll()
         player2 = dice_roll()
@@ -15,11 +17,20 @@ def main():
         if player1 == player2:
                 print("DRAW!")
         elif player1 > player2:
-                print("Player 1 wins!")
+            player1wins = player1wins + 1
+            print("Player 1 wins the round!")
         else:
-                print("Player 2 wins!")
+            player2wins = player2wins + 1
+            print("Player 2 wins the round!")
 
         rounds = rounds + 1
+
+    if player1wins == player2wins:
+            print("DRAW")
+    elif player1wins > player2wins:
+            print("Player 1 wins the game")
+    else:
+            print("Player 2 wins the game!")
 
 def dice_roll():
     diceRoll = random.randint (1,6)
