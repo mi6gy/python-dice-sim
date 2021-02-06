38d1d3d7e2e555063cc76825feea75ad78c1d374
Miguel Jimenez <mjimenez08@gmail.com>
2021-02-06 11:53:13 -0500

    added line breaks

--- diceRoll.py
+++ diceRoll.py
@@ -15,22 +15,22 @@ def main():
         print("Player 2 Roll: " + str(player2))
 
         if player1 == player2:
-                print("DRAW!")
+                print("DRAW!\n")
         elif player1 > player2:
             player1wins = player1wins + 1
-            print("Player 1 wins the round!")
+            print("Player 1 wins the round!\n")
         else:
             player2wins = player2wins + 1
-            print("Player 2 wins the round!")
+            print("Player 2 wins the round!\n")
 
         rounds = rounds + 1
 
     if player1wins == player2wins:
-            print("DRAW")
+            print("DRAW\n")
     elif player1wins > player2wins:
-            print("Player 1 wins the game")
+            print("Player 1 wins the game\n")
     else:
-            print("Player 2 wins the game!")
+            print("Player 2 wins the game!\n")
 
 def dice_roll():
     diceRoll = random.randint (1,6)
